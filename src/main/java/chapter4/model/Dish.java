e943a632038eb8a8f6d18894679f09ccbe6a7bdb
LiPenglin <529884069@qq.com>
2018-08-09 00:53:02 +0800

    [penglin] [Java8Dojo-04] add streams and parallel streams.

---
+++ src/main/java/chapter4/model/Dish.java
@@ -0,0 +1,27 @@
+package chapter4.model;
+
+public class Dish {
+    private Integer calories;
+    private String name;
+
+    public Dish(Integer calories, String name) {
+        this.calories = calories;
+        this.name = name;
+    }
+
+    public Integer getCalories() {
+        return calories;
+    }
+
+    public void setCalories(Integer calories) {
+        this.calories = calories;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
